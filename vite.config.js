4354001d121ea821de0f3e40695ca30c6a537ca9
Abdurauf <you@example.com>
2026-08-18 14:42:46 +0500

    ohhho

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['icon.png'],
       manifest: {
-        name: 'Qandchi Bola Distribyutsiya',
+        name: 'Qandchi Bola - Distribyutsiya va Kassa Tizimi',
         short_name: 'Qandchi Bola',
-        description: 'Qandchi Bola Ulgurji Distribyutsiya Tizimi',
+        description: 'Qandchi Bola - Distribyutsiya va Kassa Tizimi',
         theme_color: '#0f172a',
         background_color: '#070b13',
         display: 'fullscreen',
